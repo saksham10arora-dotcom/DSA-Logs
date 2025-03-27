f9c2a994de4e7daf4698406e2d8522ec97529c0a
Saksham Arora <saksham10arora@gmail.com>
2025-03-27 21:13:00 +0530

    Add solution for Codeforces_231A_Team.cpp

---
+++ Arrays_and_Hashing/Codeforces_231A_Team.cpp
@@ -0,0 +1,64 @@
+/**
+ * Problem: Team (Codeforces 231A)
+ * Link: https://codeforces.com/problemset/problem/231/A
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// --- Optimal Solution ---
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+void solve() {
+    int n;
+    cin >> n;
+    int count = 0;
+    while (n--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b + c >= 2) count++;
+    }
+    cout << count << endl;
+}
+
+int main() {
+    // solve();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
