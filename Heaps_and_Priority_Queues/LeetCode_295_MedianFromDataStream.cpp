c2ccd3dd861f0f220125afa3e539fdd7120964b6
Saksham Arora <saksham10arora@gmail.com>
2025-06-06 23:24:00 +0530

    Refine logic/comments in LeetCode_295_MedianFromDataStream.cpp

--- Heaps_and_Priority_Queues/LeetCode_295_MedianFromDataStream.cpp
+++ Heaps_and_Priority_Queues/LeetCode_295_MedianFromDataStream.cpp
@@ -1,34 +1,55 @@
-/**
- * Problem: Find Median from Data Stream (LeetCode 295)
- * Link: https://leetcode.com/problems/find-median-from-data-stream/
- */
+```cpp
+// Median From Data Stream, https://leetcode.com/problems/median-from-data-stream/
+// Median is the middle value in an ordered integer list. If the size of the list is even, 
+// there is no middle value.  So the median is the mean of the two middle number.
 
-#include <iostream>
-#include <queue>
-#include <vector>
+// Brute force approach: O(n log n) for sorting
+// class MedianFinder {
+//     vector<int> nums;
+// public:
+//     MedianFinder() {}
+//     void addNum(int num) {
+//         nums.push_back(num);
+//     }
+//     double findMedian() {
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         if (n % 2 == 0) {
+//             return (nums[n / 2 - 1] + nums[n / 2]) / 2.0;
+//         } else {
+//             return nums[n / 2];
+//         }
+//     }
+// };
 
-using namespace std;
-
-// --- Optimal Solution (Two Heaps) ---
-// Time Complexity: O(log N) per add, O(1) per find
-// Space Complexity: O(N)
+// Optimal solution: O(log n) for heap operations
 class MedianFinder {
-    priority_queue<int> maxHeap; // smaller half
-    priority_queue<int, vector<int>, greater<int>> minHeap; // larger half
+    priority_queue<int> maxHeap; // max heap for smaller half
+    priority_queue<int, vector<int>, greater<int>> minHeap; // min heap for larger half
 public:
+    MedianFinder() {}
     void addNum(int num) {
-        maxHeap.push(num);
-        minHeap.push(maxHeap.top());
-        maxHeap.pop();
-        if (minHeap.size() > maxHeap.size()) {
+        // Add to max heap
+        if (maxHeap.empty() || num <= maxHeap.top()) {
+            maxHeap.push(num);
+        } else {
+            minHeap.push(num);
+        }
+        // Balance heaps
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
             maxHeap.push(minHeap.top());
             minHeap.pop();
         }
     }
-    
     double findMedian() {
-        if (maxHeap.size() > minHeap.size()) return maxHeap.top();
-        return (maxHeap.top() + minHeap.top()) / 2.0;
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        } else {
+            return maxHeap.top();
+        }
     }
 };
 
@@ -36,23 +57,18 @@ int main() {
     MedianFinder mf;
     mf.addNum(1);
     mf.addNum(2);
-    cout << "Median: " << mf.findMedian() << endl; // 1.5
+    printf("%.1f\n", mf.findMedian()); // 1.5
     mf.addNum(3);
-    cout << "Median: " << mf.findMedian() << endl; // 2.0
+    printf("%.1f\n", mf.findMedian()); // 2.0
+
+    MedianFinder mf2;
+    mf2.addNum(-1);
+    mf2.addNum(-2);
+    mf2.addNum(-3);
+    mf2.addNum(-4);
+    mf2.addNum(-5);
+    printf("%.1f\n", mf2.findMedian()); // -3.0
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+```
