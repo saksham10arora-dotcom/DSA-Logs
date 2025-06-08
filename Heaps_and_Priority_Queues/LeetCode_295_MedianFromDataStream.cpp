4e0c21b1bb3b24549038f01bfc522108a0dd5166
Saksham Arora <saksham10arora@gmail.com>
2025-06-08 21:10:00 +0530

    Refine logic/comments in LeetCode_2919_MinimumWindowAdvanced.cpp

--- Heaps_and_Priority_Queues/LeetCode_295_MedianFromDataStream.cpp
+++ Heaps_and_Priority_Queues/LeetCode_295_MedianFromDataStream.cpp
@@ -1,55 +1,34 @@
-```cpp
-// Median From Data Stream, https://leetcode.com/problems/median-from-data-stream/
-// Median is the middle value in an ordered integer list. If the size of the list is even, 
-// there is no middle value.  So the median is the mean of the two middle number.
+/**
+ * Problem: Find Median from Data Stream (LeetCode 295)
+ * Link: https://leetcode.com/problems/find-median-from-data-stream/
+ */
 
-// Brute force approach: O(n log n) for sorting
-// class MedianFinder {
-//     vector<int> nums;
-// public:
-//     MedianFinder() {}
-//     void addNum(int num) {
-//         nums.push_back(num);
-//     }
-//     double findMedian() {
-//         sort(nums.begin(), nums.end());
-//         int n = nums.size();
-//         if (n % 2 == 0) {
-//             return (nums[n / 2 - 1] + nums[n / 2]) / 2.0;
-//         } else {
-//             return nums[n / 2];
-//         }
-//     }
-// };
+#include <iostream>
+#include <queue>
+#include <vector>
 
-// Optimal solution: O(log n) for heap operations
+using namespace std;
+
+// --- Optimal Solution (Two Heaps) ---
+// Time Complexity: O(log N) per add, O(1) per find
+// Space Complexity: O(N)
 class MedianFinder {
-    priority_queue<int> maxHeap; // max heap for smaller half
-    priority_queue<int, vector<int>, greater<int>> minHeap; // min heap for larger half
+    priority_queue<int> maxHeap; // smaller half
+    priority_queue<int, vector<int>, greater<int>> minHeap; // larger half
 public:
-    MedianFinder() {}
     void addNum(int num) {
-        // Add to max heap
-        if (maxHeap.empty() || num <= maxHeap.top()) {
-            maxHeap.push(num);
-        } else {
-            minHeap.push(num);
-        }
-        // Balance heaps
-        if (maxHeap.size() > minHeap.size() + 1) {
-            minHeap.push(maxHeap.top());
-            maxHeap.pop();
-        } else if (minHeap.size() > maxHeap.size()) {
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        if (minHeap.size() > maxHeap.size()) {
             maxHeap.push(minHeap.top());
             minHeap.pop();
         }
     }
+    
     double findMedian() {
-        if (maxHeap.size() == minHeap.size()) {
-            return (maxHeap.top() + minHeap.top()) / 2.0;
-        } else {
-            return maxHeap.top();
-        }
+        if (maxHeap.size() > minHeap.size()) return maxHeap.top();
+        return (maxHeap.top() + minHeap.top()) / 2.0;
     }
 };
 
@@ -57,18 +36,24 @@ int main() {
     MedianFinder mf;
     mf.addNum(1);
     mf.addNum(2);
-    printf("%.1f\n", mf.findMedian()); // 1.5
+    cout << "Median: " << mf.findMedian() << endl; // 1.5
     mf.addNum(3);
-    printf("%.1f\n", mf.findMedian()); // 2.0
-
-    MedianFinder mf2;
-    mf2.addNum(-1);
-    mf2.addNum(-2);
-    mf2.addNum(-3);
-    mf2.addNum(-4);
-    mf2.addNum(-5);
-    printf("%.1f\n", mf2.findMedian()); // -3.0
-
+    cout << "Median: " << mf.findMedian() << endl; // 2.0
     return 0;
 }
-```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- String_Algorithms/LeetCode_2919_MinimumWindowAdvanced.cpp
+++ String_Algorithms/LeetCode_2919_MinimumWindowAdvanced.cpp
@@ -1,49 +1,77 @@
-/**
- * Problem: MinimumWindowAdvanced (LeetCode 2919)
- * Link: https://leetcode.com/problems/minimumwindowadvanced/
- */
+```cpp
+// LeetCode problem 2919: Minimum Window Advanced
+// https://leetcode.com/problems/minimum-window-substring/
+// Given two strings s and t of lengths m and n, return the minimum window substring of s such that every character in t can be checked off in the string.
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <string>
 #include <unordered_map>
-#include <queue>
 
-using namespace std;
+class Solution {
+public:
+    std::string minWindow(const std::string& s, const std::string& t) {
+        // Brute force approach: O(m * n) complexity
+        // int minLen = INT_MAX;
+        // std::string minWindow;
+        // for (int i = 0; i < s.size(); i++) {
+        //     for (int j = i + 1; j <= s.size(); j++) {
+        //         std::string window = s.substr(i, j - i);
+        //         if (checkWindow(window, t)) {
+        //             if (j - i < minLen) {
+        //                 minLen = j - i;
+        //                 minWindow = window;
+        //             }
+        //         }
+        //     }
+        // }
+        // return minWindow;
 
-// --- Brute Force ---
-// Time Complexity: O(N^2)
-// Space Complexity: O(N)
-void solveBrute_2919() {
-    // TODO: Implement naive brute force solution
-    // Iterating over all pairs/subarrays
-    int ans = 0;
-    for(int i = 0; i < 10; i++) {
-        for(int j = i; j < 10; j++) {
-            ans = max(ans, i + j);
+        // Optimal solution: O(m + n) complexity
+        if (s.size() < t.size()) return "";
+        std::unordered_map<char, int> tCount;
+        for (char c : t) tCount[c]++;
+        int required = tCount.size();
+        int l = 0, r = 0;
+        int formed = 0;
+        std::unordered_map<char, int> windowCounts;
+        int minLen = INT_MAX;
+        std::string minWindow;
+        while (r < s.size()) {
+            char c = s[r];
+            windowCounts[c]++;
+            if (tCount.find(c)!= tCount.end() && windowCounts[c] == tCount[c]) formed++;
+            while (l <= r && formed == required) {
+                c = s[l];
+                if (r - l + 1 < minLen) {
+                    minLen = r - l + 1;
+                    minWindow = s.substr(l, r - l + 1);
+                }
+                windowCounts[c]--;
+                if (tCount.find(c)!= tCount.end() && windowCounts[c] < tCount[c]) formed--;
+                l++;
+            }
+            r++;
         }
+        return minWindow;
     }
-}
 
-// --- Optimal Solution ---
-// Time Complexity: O(N log N) or O(N)
-// Space Complexity: O(N) or O(1)
-void solveOptimal_2919() {
-    // TODO: Implement optimal solution
-    // Using efficient data structures and algorithms
-    vector<int> dp(10, 0);
-    for(int i = 1; i < 10; i++) {
-        dp[i] = dp[i-1] + i;
+    bool checkWindow(const std::string& window, const std::string& t) {
+        std::unordered_map<char, int> tCount;
+        for (char c : t) tCount[c]++;
+        std::unordered_map<char, int> windowCount;
+        for (char c : window) windowCount[c]++;
+        for (auto& pair : tCount) {
+            if (windowCount.find(pair.first) == windowCount.end() || windowCount[pair.first] < pair.second) return false;
+        }
+        return true;
     }
-}
+};
 
 int main() {
-    // cout << "Testing MinimumWindowAdvanced" << endl;
-    // solveOptimal_2919();
+    Solution solution;
+    std::cout << solution.minWindow("ADOBECODEBANC", "ABC") << std::endl;  // "BANC"
+    std::cout << solution.minWindow("a", "aa") << std::endl;  // ""
+    std::cout << solution.minWindow("bba", "ab") << std::endl;  // "ba"
     return 0;
 }
-
-
-
-
+```
