f20e2fb64e33086027f1724bdea7929e8f5302cd
Saksham Arora <saksham10arora@gmail.com>
2025-06-10 20:25:00 +0530

    Refine logic/comments in LeetCode_2341_MaximumNumberOfPairsInArray.cpp

--- Arrays_and_Hashing/LeetCode_1224_MaximumEqualFrequency.cpp
+++ Arrays_and_Hashing/LeetCode_1224_MaximumEqualFrequency.cpp
@@ -1,90 +1,44 @@
-```cpp
-// Maximum Equal Frequency, https://leetcode.com/problems/maximum-equal-frequency/
-// Given an array nums of integers, for each integer nums[i] we may choose any x with 1 <= x <= 6 where nums[i]!= x, 
-// then multiply nums[i] by x. If the value of nums[i] is already x, then we must leave it as is except if doing so would cause 
-// the entire array to have all the same value, in which case we must multiply it by some other number. 
-// Return the maximum number of operations to make all elements in nums the same. In one operation, we can multiply one element 
-// of nums by an integer.
+/**
+ * Problem: Maximum Equal Frequency (LeetCode 1224)
+ * Link: https://leetcode.com/problems/maximum-equal-frequency/
+ */
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
 #include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
 
-class Solution {
-public:
-    // Brute force approach with O(n*6^n) complexity
-    // int maxEqualFreq(std::vector<int>& nums) {
-    //     int n = nums.size();
-    //     int maxFreq = 0;
-    //     for (int mask = 0; mask < (1 << n); mask++) {
-    //         std::unordered_map<int, int> freq;
-    //         for (int i = 0; i < n; i++) {
-    //             if (mask & (1 << i)) {
-    //                 freq[nums[i] * 2]++;
-    //             } else {
-    //                 freq[nums[i]]++;
-    //             }
-    //         }
-    //         if (isSame(freq)) {
-    //             maxFreq = std::max(maxFreq, __builtin_popcount(mask));
-    //         }
-    //     }
-    //     return maxFreq;
-    // }
+using namespace std;
 
-    // Optimal solution with O(n) complexity
-    int maxEqualFreq(std::vector<int>& nums) {
-        int n = nums.size();
-        std::unordered_map<int, int> freq;
-        for (int num : nums) {
-            freq[num]++;
-        }
-        int maxFreq = 0;
-        for (int num : freq) {
-            maxFreq = std::max(maxFreq, num.second);
-        }
-        if (maxFreq == 1) {
-            return n;
-        }
-        if (freq.size() == 1) {
-            return n;
-        }
-        if (freq.size() == 2) {
-            int maxCount = 0;
-            int minCount = n;
-            for (auto& pair : freq) {
-                maxCount = std::max(maxCount, pair.second);
-                minCount = std::min(minCount, pair.second);
-            }
-            if (maxCount - minCount == 1 || maxCount == 1) {
-                return n;
-            }
-        }
-        return n - 1;
-    }
+// --- Brute Force Approach ---
+// Time Complexity: O(N^2)
+// Space Complexity: O(N)
+void solveBrute() {
+    // TODO: Implement brute force
+    // A naive approach exploring all possible states
+}
 
-    bool isSame(const std::unordered_map<int, int>& freq) {
-        int count = 0;
-        for (auto& pair : freq) {
-            if (pair.second > 0) {
-                count++;
-            }
-        }
-        return count == 1;
-    }
-};
+// --- Optimal Approach ---
+// Time Complexity: O(N) or O(N log N)
+// Space Complexity: O(1) or O(N)
+void solveOptimal() {
+    // TODO: Implement optimal solution
+    // Utilize efficient data structures and algorithmic patterns
+}
 
 int main() {
-    Solution solution;
-    std::vector<int> nums1 = {2, 2, 1, 1, 5, 3, 3, 5};
-    std::cout << solution.maxEqualFreq(nums1) << std::endl;  // Output: 7
-
-    std::vector<int> nums2 = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5};
-    std::cout << solution.maxEqualFreq(nums2) << std::endl;  // Output: 13
-
-    std::vector<int> nums3 = {1, 1, 1, 2, 2, 2};
-    std::cout << solution.maxEqualFreq(nums3) << std::endl;  // Output: 5
-
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // cout << "Testing Maximum Equal Frequency" << endl;
+    // solveOptimal();
+    
     return 0;
 }
-```
+
+
