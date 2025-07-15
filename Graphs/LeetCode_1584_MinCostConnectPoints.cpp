53d0b938a55aed1dac5322d1abb51119e8066736
Saksham Arora <saksham10arora@gmail.com>
2025-07-15 22:08:00 +0530

    Add solution for LeetCode_1348_SameTreeII.cpp

--- Graphs/LeetCode_1584_MinCostConnectPoints.cpp
+++ Graphs/LeetCode_1584_MinCostConnectPoints.cpp
@@ -1,89 +1,71 @@
-```cpp
-// LeetCode problem 1584: Min Cost Connect Points, https://leetcode.com/problems/min-cost-to-connect-all-points/
-// Given an array of points where points[i] = [xi, yi] represents a point on a 2D plane, find the minimum cost to connect all the points.
+/**
+ * Problem: Min Cost to Connect All Points (LeetCode 1584)
+ * Link: https://leetcode.com/problems/min-cost-to-connect-all-points/
+ */
 
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cmath>
 #include <algorithm>
 
-// Brute force approach: Try all possible connections between points, O(n^2 * n!) complexity
-// int minCostConnectPoints(std::vector<std::vector<int>>& points) {
-//     int n = points.size();
-//     int result = INT_MAX;
-//     std::vector<int> permutation(n);
-//     for (int i = 0; i < n; i++) permutation[i] = i;
-//     do {
-//         int cost = 0;
-//         for (int i = 0; i < n - 1; i++) {
-//             cost += std::abs(points[permutation[i]][0] - points[permutation[i + 1]][0]) + std::abs(points[permutation[i]][1] - points[permutation[i + 1]][1]);
-//         }
-//         result = std::min(result, cost);
-//     } while (std::next_permutation(permutation.begin(), permutation.end()));
-//     return result;
-// }
+using namespace std;
 
-// Optimal solution: Use Kruskal's algorithm to find the minimum spanning tree, O(n^2 * log(n)) complexity
-class UnionFind {
-public:
-    std::vector<int> parent;
-    UnionFind(int n) : parent(n) {
-        for (int i = 0; i < n; i++) parent[i] = i;
-    }
-    int find(int x) {
-        if (parent[x] != x) parent[x] = find(parent[x]);
-        return parent[x];
-    }
-    void unionSet(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX != rootY) parent[rootX] = rootY;
-    }
-};
-
-struct Edge {
-    int cost;
-    int x;
-    int y;
-    bool operator<(const Edge& other) const {
-        return cost > other.cost;
-    }
-};
-
-int minCostConnectPoints(std::vector<std::vector<int>>& points) {
+// --- Optimal Solution (Prim's Algorithm) ---
+// Time Complexity: O(E log V) = O(N^2 log N)
+// Space Complexity: O(V^2)
+int minCostConnectPoints(vector<vector<int>>& points) {
     int n = points.size();
-    std::priority_queue<Edge> edges;
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            int cost = std::abs(points[i][0] - points[j][0]) + std::abs(points[i][1] - points[j][1]);
-            edges.push({cost, i, j});
+    vector<bool> inMST(n, false);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, 0});
+    
+    int cost = 0, edges = 0;
+    while (edges < n) {
+        int d = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+        
+        if (inMST[u]) continue;
+        inMST[u] = true;
+        cost += d;
+        edges++;
+        
+        for (int v = 0; v < n; v++) {
+            if (!inMST[v]) {
+                int dist = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]);
+                pq.push({dist, v});
+            }
         }
     }
-    UnionFind uf(n);
-    int result = 0;
-    int count = 0;
-    while (!edges.empty() && count < n - 1) {
-        Edge edge = edges.top();
-        edges.pop();
-        if (uf.find(edge.x) != uf.find(edge.y)) {
-            uf.unionSet(edge.x, edge.y);
-            result += edge.cost;
-            count++;
-        }
-    }
-    return result;
+    return cost;
 }
 
 int main() {
-    std::vector<std::vector<int>> points1 = {{0, 0}, {2, 2}, {3, 10}, {5, 2}, {7, 0}};
-    std::cout << minCostConnectPoints(points1) << std::endl;  // Output: 20
-
-    std::vector<std::vector<int>> points2 = {{3, 12}, {-7, 3}, {-13, 9}, {15, -7}, {5, -10}, {10, 4}};
-    std::cout << minCostConnectPoints(points2) << std::endl;  // Output: 34
-
-    std::vector<std::vector<int>> points3 = {{0, 0}, {1, 1}, {1, 0}, {0, 1}};
-    std::cout << minCostConnectPoints(points3) << std::endl;  // Output: 2
-
+    vector<vector<int>> points = {{0, 0}, {2, 2}, {3, 10}, {5, 2}, {7, 0}};
+    cout << "Min Cost: " << minCostConnectPoints(points) << endl;
     return 0;
 }
-```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
