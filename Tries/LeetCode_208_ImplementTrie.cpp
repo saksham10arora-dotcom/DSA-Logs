a03b7fd211a1db0a9bc7275e7d96a74ecf60d944
Saksham Arora <saksham10arora@gmail.com>
2025-07-18 22:53:00 +0530

    Add solution for LeetCode_208_ImplementTrie.cpp

---
+++ Tries/LeetCode_208_ImplementTrie.cpp
@@ -0,0 +1,72 @@
+/**
+ * Problem: Implement Trie (Prefix Tree) (LeetCode 208)
+ * Link: https://leetcode.com/problems/implement-trie-prefix-tree/
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node {
+    Node* links[26];
+    bool flag = false;
+    bool containsKey(char ch) { return links[ch - 'a'] != NULL; }
+    void put(char ch, Node* node) { links[ch - 'a'] = node; }
+    Node* get(char ch) { return links[ch - 'a']; }
+    void setEnd() { flag = true; }
+    bool isEnd() { return flag; }
+};
+
+class Trie {
+    Node* root;
+public:
+    Trie() { root = new Node(); }
+    
+    void insert(string word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containsKey(ch)) {
+                node->put(ch, new Node());
+            }
+            node = node->get(ch);
+        }
+        node->setEnd();
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containsKey(ch)) return false;
+            node = node->get(ch);
+        }
+        return node->isEnd();
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for (char ch : prefix) {
+            if (!node->containsKey(ch)) return false;
+            node = node->get(ch);
+        }
+        return true;
+    }
+};
+
+int main() {
+    Trie trie;
+    trie.insert("apple");
+    cout << trie.search("apple") << endl;   // true
+    cout << trie.search("app") << endl;     // false
+    cout << trie.startsWith("app") << endl; // true
+    return 0;
+}
+
+
+
+
+
+
+
+
+
