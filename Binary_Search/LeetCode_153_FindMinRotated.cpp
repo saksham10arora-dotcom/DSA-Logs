18bd208cde9278787c2f895eab0dc5a9232037af
Saksham Arora <saksham10arora@gmail.com>
2025-07-20 13:57:00 +0530

    Add solution for LeetCode_153_FindMinRotated.cpp

---
+++ Binary_Search/LeetCode_153_FindMinRotated.cpp
@@ -0,0 +1,72 @@
+/**
+ * Problem: Find Minimum in Rotated Sorted Array (LeetCode 153)
+ * Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// --- Optimal Solution ---
+// Time Complexity: O(log N)
+// Space Complexity: O(1)
+int findMin(vector<int>& nums) {
+    int low = 0, high = nums.size() - 1;
+    int ans = nums[0];
+    while (low <= high) {
+        if (nums[low] <= nums[high]) {
+            ans = min(ans, nums[low]);
+            break;
+        }
+        int mid = low + (high - low) / 2;
+        ans = min(ans, nums[mid]);
+        if (nums[mid] >= nums[low]) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {3, 4, 5, 1, 2};
+    cout << "Min: " << findMin(nums) << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
