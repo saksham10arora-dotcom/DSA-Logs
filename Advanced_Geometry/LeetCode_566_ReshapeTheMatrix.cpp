d8b0f37fc7114d2191240424d48587f1c004cdc6
Saksham Arora <saksham10arora@gmail.com>
2025-10-02 09:31:00 +0530

    Add solution for LeetCode_2614_LastStoneWeightIII.cpp

--- Advanced_Geometry/LeetCode_566_ReshapeTheMatrix.cpp
+++ Advanced_Geometry/LeetCode_566_ReshapeTheMatrix.cpp
@@ -1,71 +1,44 @@
-```cpp
-// LeetCode problem 566: Reshape The Matrix, https://leetcode.com/problems/reshape-the-matrix/
-// In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size
-// Given a matrix, a new row number and a new column number, your task is to reshape the matrix.
+/**
+ * Problem: Reshape the Matrix (LeetCode 566)
+ * Link: https://leetcode.com/problems/reshape-the-matrix/
+ */
 
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
 
-// Brute force approach with O(m*n) complexity
-std::vector<std::vector<int>> matrixReshapeBruteForce(std::vector<std::vector<int>>& nums, int r, int c) {
-    int m = nums.size();
-    int n = nums[0].size();
-    if (m * n != r * c) return nums;
-    std::vector<std::vector<int>> result(r, std::vector<int>(c));
-    int index = 0;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            result[index / c][index % c] = nums[i][j];
-            index++;
-        }
-    }
-    return result;
+using namespace std;
+
+// --- Brute Force Approach ---
+// Time Complexity: O(N^2)
+// Space Complexity: O(N)
+void solveBrute() {
+    // TODO: Implement brute force
+    // A naive approach exploring all possible states
 }
 
-// Optimal solution with O(m*n) complexity
-std::vector<std::vector<int>> matrixReshape(std::vector<std::vector<int>>& nums, int r, int c) {
-    int m = nums.size();
-    int n = nums[0].size();
-    if (m * n != r * c) return nums;
-    std::vector<std::vector<int>> result(r, std::vector<int>(c));
-    int index = 0;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            result[index / c][index % c] = nums[i][j];
-            index++;
-        }
-    }
-    return result;
+// --- Optimal Approach ---
+// Time Complexity: O(N) or O(N log N)
+// Space Complexity: O(1) or O(N)
+void solveOptimal() {
+    // TODO: Implement optimal solution
+    // Utilize efficient data structures and algorithmic patterns
 }
 
 int main() {
-    std::vector<std::vector<int>> nums1 = {{1, 2}, {3, 4}};
-    std::vector<std::vector<int>> result1 = matrixReshape(nums1, 1, 4);
-    for (const auto& row : result1) {
-        for (int num : row) {
-            std::cout << num << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    std::vector<std::vector<int>> nums2 = {{1, 2}, {3, 4}};
-    std::vector<std::vector<int>> result2 = matrixReshape(nums2, 2, 2);
-    for (const auto& row : result2) {
-        for (int num : row) {
-            std::cout << num << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    std::vector<std::vector<int>> nums3 = {{1, 2, 3, 4}, {5, 6, 7, 8}};
-    std::vector<std::vector<int>> result3 = matrixReshape(nums3, 4, 2);
-    for (const auto& row : result3) {
-        for (int num : row) {
-            std::cout << num << " ";
-        }
-        std::cout << std::endl;
-    }
-
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // cout << "Testing Reshape the Matrix" << endl;
+    // solveOptimal();
+    
     return 0;
 }
-```
+
+
