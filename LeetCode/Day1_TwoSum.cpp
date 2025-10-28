41c5f85fd9f80d72f18230a544b659e38b15ba73
C0SBYYY <saksham10arora@gmail.com>
2025-10-28 14:47:18 +0530

    Implement twoSum function using unordered_map

--- LeetCode/Day1_TwoSum.cpp
+++ LeetCode/Day1_TwoSum.cpp
@@ -1,1 +1,19 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map; // Key: number, Value: index
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (map.find(complement) != map.end()) {
+                return {map[complement], i};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
 
