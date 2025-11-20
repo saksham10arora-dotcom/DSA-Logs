b7d31457d0bfd30f08172eb2da50b0106c688a21
Saksham Arora <saksham10arora@gmail.com>
2025-11-20 21:31:00 +0530

    Refine logic/comments in LeetCode_1144_MinWindowSubstringOptimized.cpp

--- Sliding_Window/LeetCode_1144_MinWindowSubstringOptimized.cpp
+++ Sliding_Window/LeetCode_1144_MinWindowSubstringOptimized.cpp
@@ -1,48 +1,54 @@
-/**
- * Problem: MinWindowSubstringOptimized (LeetCode 1144)
- * Link: https://leetcode.com/problems/minwindowsubstringoptimized/
- */
+```cpp
+// LeetCode problem 1144: Decrease Elements To Make Array Zigzag
+// https://leetcode.com/problems/decrease-elements-to-make-array-zigzag/
+// Given an array nums, we need to decrease elements to make array zigzag.
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <unordered_map>
-#include <queue>
+// Brute force approach with O(n^2) complexity
+// class Solution {
+// public:
+//     int movesToMakeZigzag(vector<int>& nums) {
+//         int n = nums.size();
+//         int res = INT_MAX;
+//         for (int i = 0; i < n; i++) {
+//             int tmp = 0;
+//             for (int j = 0; j < n; j++) {
+//                 if ((j - i) % 2 == 0 && (j > 0 && nums[j] >= nums[j - 1] || j < n - 1 && nums[j] >= nums[j + 1])) {
+//                     tmp++;
+//                 }
+//             }
+//             res = min(res, tmp);
+//         }
+//         return res;
+//     }
+// };
 
-using namespace std;
-
-// --- Brute Force ---
-// Time Complexity: O(N^2)
-// Space Complexity: O(N)
-void solveBrute_1144() {
-    // TODO: Implement naive brute force solution
-    // Iterating over all pairs/subarrays
-    int ans = 0;
-    for(int i = 0; i < 10; i++) {
-        for(int j = i; j < 10; j++) {
-            ans = max(ans, i + j);
+// Optimal solution with O(n) complexity
+class Solution {
+public:
+    int movesToMakeZigzag(vector<int>& nums) {
+        int n = nums.size();
+        int res = INT_MAX;
+        for (int parity = 0; parity < 2; parity++) {
+            int tmp = 0;
+            for (int i = 0; i < n; i++) {
+                if ((i % 2 == parity) && (i > 0 && nums[i] >= nums[i - 1] || i < n - 1 && nums[i] >= nums[i + 1])) {
+                    tmp++;
+                }
+            }
+            res = min(res, tmp);
         }
+        return res;
     }
-}
-
-// --- Optimal Solution ---
-// Time Complexity: O(N log N) or O(N)
-// Space Complexity: O(N) or O(1)
-void solveOptimal_1144() {
-    // TODO: Implement optimal solution
-    // Using efficient data structures and algorithms
-    vector<int> dp(10, 0);
-    for(int i = 1; i < 10; i++) {
-        dp[i] = dp[i-1] + i;
-    }
-}
+};
 
 int main() {
-    // cout << "Testing MinWindowSubstringOptimized" << endl;
-    // solveOptimal_1144();
+    Solution solution;
+    vector<int> nums1 = {1, 2, 3};
+    vector<int> nums2 = {9, 6, 1, 6, 2};
+    vector<int> nums3 = {1, 2, 3, 4, 5};
+    cout << solution.movesToMakeZigzag(nums1) << endl;  // Output: 2
+    cout << solution.movesToMakeZigzag(nums2) << endl;  // Output: 4
+    cout << solution.movesToMakeZigzag(nums3) << endl;  // Output: 2
     return 0;
 }
-
-
-
+```
