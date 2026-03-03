f482dcb62cb6533b7c3623d22709ab6303717c41
Saksham Arora <saksham10arora@gmail.com>
2026-03-03 14:34:00 +0530

    Add solution for LeetCode_78_Subsets.cpp

---
+++ Recursion_and_Backtracking/LeetCode_78_Subsets.cpp
@@ -0,0 +1,55 @@
+/**
+ * Problem: Subsets (LeetCode 78)
+ * Link: https://leetcode.com/problems/subsets/
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// --- Optimal Solution (Backtracking) ---
+// Time Complexity: O(N * 2^N)
+// Space Complexity: O(N)
+void backtrack(int start, vector<int>& nums, vector<int>& current, vector<vector<int>>& res) {
+    res.push_back(current);
+    for (int i = start; i < nums.size(); i++) {
+        current.push_back(nums[i]);
+        backtrack(i + 1, nums, current, res);
+        current.pop_back(); // backtrack
+    }
+}
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> res;
+    vector<int> current;
+    backtrack(0, nums, current, res);
+    return res;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> res = subsets(nums);
+    for (auto& subset : res) {
+        cout << "[ ";
+        for (int x : subset) cout << x << " ";
+        cout << "]" << endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
