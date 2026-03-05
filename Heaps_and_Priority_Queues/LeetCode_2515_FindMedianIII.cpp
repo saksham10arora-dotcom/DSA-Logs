45525544a77b3d5ba9e76ea5772815d5f99cf43c
Saksham Arora <saksham10arora@gmail.com>
2026-03-05 19:39:00 +0530

    Refine logic/comments in LeetCode_2515_FindMedianIII.cpp

--- Heaps_and_Priority_Queues/LeetCode_2515_FindMedianIII.cpp
+++ Heaps_and_Priority_Queues/LeetCode_2515_FindMedianIII.cpp
@@ -1,50 +1,57 @@
-/**
- * Problem: FindMedianIII (LeetCode 2515)
- * Link: https://leetcode.com/problems/findmedianiii/
- */
+```cpp
+// LeetCode problem 2515: Find Median I I I
+// https://leetcode.com/problems/find-median-iii/
+// Given an array of integers, find the median of the array.
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <unordered_map>
 #include <queue>
+#include <vector>
+#include <iostream>
 
-using namespace std;
-
-// --- Brute Force ---
-// Time Complexity: O(N^2)
-// Space Complexity: O(N)
-void solveBrute_2515() {
-    // TODO: Implement naive brute force solution
-    // Iterating over all pairs/subarrays
-    int ans = 0;
-    for(int i = 0; i < 10; i++) {
-        for(int j = i; j < 10; j++) {
-            ans = max(ans, i + j);
+// Brute force approach: sorting the array, O(n log n)
+class Solution_Brute_Force {
+public:
+    double findMedian(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int n = nums.size();
+        if (n % 2 == 0) {
+            return (nums[n / 2 - 1] + nums[n / 2]) / 2.0;
+        } else {
+            return nums[n / 2];
         }
     }
-}
+};
 
-// --- Optimal Solution ---
-// Time Complexity: O(N log N) or O(N)
-// Space Complexity: O(N) or O(1)
-void solveOptimal_2515() {
-    // TODO: Implement optimal solution
-    // Using efficient data structures and algorithms
-    vector<int> dp(10, 0);
-    for(int i = 1; i < 10; i++) {
-        dp[i] = dp[i-1] + i;
+// Optimal solution: using priority queue (max heap and min heap), O(n log n)
+class Solution {
+public:
+    double findMedian(vector<int>& nums) {
+        std::priority_queue<int> maxHeap;
+        std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+        for (int num : nums) {
+            maxHeap.push(num);
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+            if (maxHeap.size() < minHeap.size()) {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+            }
+        }
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        } else {
+            return maxHeap.top();
+        }
     }
-}
+};
 
 int main() {
-    // cout << "Testing FindMedianIII" << endl;
-    // solveOptimal_2515();
+    Solution solution;
+    std::vector<int> nums1 = {1, 2, 3, 4, 5};
+    std::vector<int> nums2 = {1, 2, 3, 4};
+    std::vector<int> nums3 = {1, 3, 5, 7, 9};
+    std::cout << solution.findMedian(nums1) << std::endl;  // Output: 3
+    std::cout << solution.findMedian(nums2) << std::endl;  // Output: 2.5
+    std::cout << solution.findMedian(nums3) << std::endl;  // Output: 5
     return 0;
 }
-
-
-
-
-
+```
