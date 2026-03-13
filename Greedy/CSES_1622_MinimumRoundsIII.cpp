94afe41d5207df312415309e8c23a2f2919da054
Saksham Arora <saksham10arora@gmail.com>
2026-03-13 21:36:00 +0530

    Refine logic/comments in CSES_1622_MinimumRoundsIII.cpp

--- Greedy/CSES_1622_MinimumRoundsIII.cpp
+++ Greedy/CSES_1622_MinimumRoundsIII.cpp
@@ -1,52 +1,51 @@
-/**
- * Problem: MinimumRoundsIII (CSES 1622)
- * Link: https://cses.com/problems/minimumroundsiii/
- */
+```cpp
+// Minimum Rounds I I I, https://cses.fi/problemset/task/1622, 
+// Given a list of n integers, find the minimum number of rounds to make all numbers equal.
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <string>
-#include <unordered_map>
-#include <queue>
 
-using namespace std;
+// Brute force approach with O(2^n) complexity
+// int minRoundsBruteForce(const std::vector<int>& nums) {
+//     int n = nums.size();
+//     int minRounds = n;
+//     for (int mask = 0; mask < (1 << n); ++mask) {
+//         std::vector<int> temp = nums;
+//         int rounds = 0;
+//         for (int i = 0; i < n; ++i) {
+//             if ((mask & (1 << i))!= 0) {
+//                 temp[i]++;
+//                 rounds++;
+//             }
+//         }
+//         if (std::equal(temp.begin() + 1, temp.end(), temp.begin())) {
+//             minRounds = std::min(minRounds, rounds);
+//         }
+//     }
+//     return minRounds;
+// }
 
-// --- Brute Force ---
-// Time Complexity: O(N^2)
-// Space Complexity: O(N)
-void solveBrute_1622() {
-    // TODO: Implement naive brute force solution
-    // Iterating over all pairs/subarrays
-    int ans = 0;
-    for(int i = 0; i < 10; i++) {
-        for(int j = i; j < 10; j++) {
-            ans = max(ans, i + j);
-        }
+// Optimal solution with O(n) complexity
+int minRounds(const std::vector<int>& nums) {
+    int n = nums.size();
+    std::sort(nums.begin(), nums.end());
+    int minRounds = 0;
+    for (int i = 1; i < n; ++i) {
+        minRounds += nums[i] - nums[0];
     }
-}
-
-// --- Optimal Solution ---
-// Time Complexity: O(N log N) or O(N)
-// Space Complexity: O(N) or O(1)
-void solveOptimal_1622() {
-    // TODO: Implement optimal solution
-    // Using efficient data structures and algorithms
-    vector<int> dp(10, 0);
-    for(int i = 1; i < 10; i++) {
-        dp[i] = dp[i-1] + i;
-    }
+    return minRounds;
 }
 
 int main() {
-    // cout << "Testing MinimumRoundsIII" << endl;
-    // solveOptimal_1622();
+    std::vector<int> test1 = {1, 2, 3};
+    std::vector<int> test2 = {4, 5, 6};
+    std::vector<int> test3 = {10, 10, 10};
+
+    std::cout << minRounds(test1) << std::endl;  // Output: 2
+    std::cout << minRounds(test2) << std::endl;  // Output: 2
+    std::cout << minRounds(test3) << std::endl;  // Output: 0
+
     return 0;
 }
-
-
-
-
-
-
-
+```
