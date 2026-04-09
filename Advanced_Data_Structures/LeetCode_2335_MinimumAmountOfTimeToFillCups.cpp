c0eacf803799e3df75609dcb7ccbddb687248c15
Saksham Arora <saksham10arora@gmail.com>
2026-04-09 20:25:00 +0530

    Refine logic/comments in LeetCode_2335_MinimumAmountOfTimeToFillCups.cpp

--- Advanced_Data_Structures/LeetCode_2335_MinimumAmountOfTimeToFillCups.cpp
+++ Advanced_Data_Structures/LeetCode_2335_MinimumAmountOfTimeToFillCups.cpp
@@ -1,43 +1,72 @@
-/**
- * Problem: Minimum Amount of Time to Fill Cups (LeetCode 2335)
- * Link: https://leetcode.com/problems/minimum-amount-of-time-to-fill-cups/
- */
+```cpp
+// LeetCode problem 2335: Minimum Amount Of Time To Fill Cups
+// https://leetcode.com/problems/minimum-amount-of-time-to-fill-cups/
+// In this problem, we are given an array of integers representing the number of cups that need to be filled.
+// We have two types of water coolers: one that fills one cup in 1 minute and another that fills two cups in 2 minutes.
+// The goal is to find the minimum amount of time required to fill all the cups.
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
 #include <queue>
-#include <stack>
+#include <vector>
 
-using namespace std;
+// Brute force approach with O(2^n) complexity
+// This approach is not efficient and will result in a time limit exceeded error for large inputs.
+// int fillCups(std::vector<int>& amount) {
+//     int n = amount.size();
+//     int res = 0;
+//     std::function<void(int, int)> dfs = [&](int idx, int time) {
+//         if (idx == n) {
+//             res = std::max(res, time);
+//             return;
+//         }
+//         dfs(idx + 1, time + amount[idx]);
+//         if (idx + 1 < n) {
+//             dfs(idx + 2, time + amount[idx] + amount[idx + 1]);
+//         }
+//     };
+//     dfs(0, 0);
+//     return res;
+// }
 
-// --- Brute Force Approach ---
-// Time Complexity: O(N^2)
-// Space Complexity: O(N)
-void solveBrute() {
-    // TODO: Implement brute force
-    // A naive approach exploring all possible states
-}
-
-// --- Optimal Approach ---
-// Time Complexity: O(N) or O(N log N)
-// Space Complexity: O(1) or O(N)
-void solveOptimal() {
-    // TODO: Implement optimal solution
-    // Utilize efficient data structures and algorithmic patterns
+// Optimal solution with O(n log n) complexity
+int fillCups(std::vector<int>& amount) {
+    std::priority_queue<int> pq;
+    for (int num : amount) {
+        pq.push(num);
+    }
+    int res = 0;
+    while (!pq.empty()) {
+        int a = pq.top();
+        pq.pop();
+        if (pq.empty()) {
+            res += a;
+            break;
+        }
+        int b = pq.top();
+        pq.pop();
+        res += 1;
+        a -= 1;
+        b -= 1;
+        if (a > 0) {
+            pq.push(a);
+        }
+        if (b > 0) {
+            pq.push(b);
+        }
+    }
+    return res;
 }
 
 int main() {
-    // Fast I/O
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    // cout << "Testing Minimum Amount of Time to Fill Cups" << endl;
-    // solveOptimal();
-    
+    std::vector<int> amount1 = {1, 2, 3};
+    std::cout << fillCups(amount1) << std::endl;  // Output: 4
+
+    std::vector<int> amount2 = {1, 2, 3, 4};
+    std::cout << fillCups(amount2) << std::endl;  // Output: 7
+
+    std::vector<int> amount3 = {5, 4, 4};
+    std::cout << fillCups(amount3) << std::endl;  // Output: 9
+
     return 0;
 }
-
+```
