8d63704da70f070c986616306e1347207305cebc
Saksham Arora <saksham10arora@gmail.com>
2026-05-06 18:12:00 +0530

    Refine logic/comments in CSES_2097_LastStoneWeightOptimized.cpp

--- Heaps_and_Priority_Queues/CSES_2097_LastStoneWeightOptimized.cpp
+++ Heaps_and_Priority_Queues/CSES_2097_LastStoneWeightOptimized.cpp
@@ -1,47 +1,80 @@
-/**
- * Problem: LastStoneWeightOptimized (CSES 2097)
- * Link: https://cses.com/problems/laststoneweightoptimized/
- */
+```cpp
+// Last Stone Weight Optimized, https://cses.fi/problemset/task/2097, 
+// Given a list of stones with different weights, remove stones in pairs of the two heaviest stones until only one or no stones are left.
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <unordered_map>
 #include <queue>
+#include <vector>
 
-using namespace std;
+// Brute force approach with O(n^2) complexity
+// std::vector<int> lastStoneWeight(std::vector<int>& stones) {
+//     while (stones.size() > 1) {
+//         int max1 = 0, max2 = 0, max1Index = 0, max2Index = 0;
+//         for (int i = 0; i < stones.size(); i++) {
+//             if (stones[i] > max1) {
+//                 max2 = max1;
+//                 max2Index = max1Index;
+//                 max1 = stones[i];
+//                 max1Index = i;
+//             } else if (stones[i] > max2) {
+//                 max2 = stones[i];
+//                 max2Index = i;
+//             }
+//         }
+//         stones.erase(stones.begin() + max1Index);
+//         stones.erase(stones.begin() + max2Index - (max1Index < max2Index));
+//         if (max1 != max2) {
+//             stones.push_back(max1 - max2);
+//         }
+//     }
+//     return stones;
+// }
 
-// --- Brute Force ---
-// Time Complexity: O(N^2)
-// Space Complexity: O(N)
-void solveBrute_2097() {
-    // TODO: Implement naive brute force solution
-    // Iterating over all pairs/subarrays
-    int ans = 0;
-    for(int i = 0; i < 10; i++) {
-        for(int j = i; j < 10; j++) {
-            ans = max(ans, i + j);
+// Optimal solution with O(n log n) complexity
+std::vector<int> lastStoneWeight(std::vector<int>& stones) {
+    std::priority_queue<int> pq;
+    for (int stone : stones) {
+        pq.push(stone);
+    }
+    while (pq.size() > 1) {
+        int x = pq.top();
+        pq.pop();
+        int y = pq.top();
+        pq.pop();
+        if (x != y) {
+            pq.push(x - y);
         }
     }
-}
-
-// --- Optimal Solution ---
-// Time Complexity: O(N log N) or O(N)
-// Space Complexity: O(N) or O(1)
-void solveOptimal_2097() {
-    // TODO: Implement optimal solution
-    // Using efficient data structures and algorithms
-    vector<int> dp(10, 0);
-    for(int i = 1; i < 10; i++) {
-        dp[i] = dp[i-1] + i;
+    std::vector<int> result;
+    while (!pq.empty()) {
+        result.push_back(pq.top());
+        pq.pop();
     }
+    return result;
 }
 
 int main() {
-    // cout << "Testing LastStoneWeightOptimized" << endl;
-    // solveOptimal_2097();
+    std::vector<int> stones1 = {2, 7, 4, 1, 8, 1};
+    std::vector<int> result1 = lastStoneWeight(stones1);
+    for (int stone : result1) {
+        std::cout << stone << " ";
+    }
+    std::cout << std::endl;
+
+    std::vector<int> stones2 = {1, 2};
+    std::vector<int> result2 = lastStoneWeight(stones2);
+    for (int stone : result2) {
+        std::cout << stone << " ";
+    }
+    std::cout << std::endl;
+
+    std::vector<int> stones3 = {1, 1, 1, 1};
+    std::vector<int> result3 = lastStoneWeight(stones3);
+    for (int stone : result3) {
+        std::cout << stone << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
-
-
+```
