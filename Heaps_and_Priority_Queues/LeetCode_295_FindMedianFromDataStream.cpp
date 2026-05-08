12eefcd2a7f07209cef16212bbb3a8133f52ee81
Saksham Arora <saksham10arora@gmail.com>
2026-05-08 23:41:00 +0530

    Refine logic/comments in LeetCode_406_QueueReconstructionByHeight.cpp

--- Heaps_and_Priority_Queues/LeetCode_295_FindMedianFromDataStream.cpp
+++ Heaps_and_Priority_Queues/LeetCode_295_FindMedianFromDataStream.cpp
@@ -1,67 +1,44 @@
-```cpp
-// LeetCode problem 295: Find Median From Data Stream, https://leetcode.com/problems/find-median-from-data-stream/
-// Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. 
-// In this case, the median is the mean of the two middle values, which is (list[listSize / 2 - 1] + list[listSize / 2]) / 2.
+/**
+ * Problem: Find Median from Data Stream (LeetCode 295)
+ * Link: https://leetcode.com/problems/find-median-from-data-stream/
+ */
 
-// Brute force approach: sorting the data stream after each insertion, O(n log n) for sorting, O(n) for insertion, O(n) for finding median
-// class MedianFinder {
-// public:
-//     vector<int> data;
-//     MedianFinder() {}
-//     void addNum(int num) {
-//         data.push_back(num);
-//         sort(data.begin(), data.end());
-//     }
-//     double findMedian() {
-//         int n = data.size();
-//         if (n % 2 == 0) {
-//             return (data[n / 2 - 1] + data[n / 2]) / 2.0;
-//         } else {
-//             return data[n / 2];
-//         }
-//     }
-// };
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
 
-// Optimal solution: using two heaps, max heap for the smaller half and min heap for the larger half, O(log n) for insertion and finding median
-class MedianFinder {
-public:
-    priority_queue<int> maxHeap; // max heap for the smaller half
-    priority_queue<int, vector<int>, greater<int>> minHeap; // min heap for the larger half
-    MedianFinder() {}
-    void addNum(int num) {
-        // add num to the correct heap
-        if (maxHeap.empty() || num <= maxHeap.top()) {
-            maxHeap.push(num);
-        } else {
-            minHeap.push(num);
-        }
-        // balance the heaps
-        if (maxHeap.size() > minHeap.size() + 1) {
-            minHeap.push(maxHeap.top());
-            maxHeap.pop();
-        } else if (minHeap.size() > maxHeap.size()) {
-            maxHeap.push(minHeap.top());
-            minHeap.pop();
-        }
-    }
-    double findMedian() {
-        if (maxHeap.size() == minHeap.size()) {
-            return (maxHeap.top() + minHeap.top()) / 2.0;
-        } else {
-            return maxHeap.top();
-        }
-    }
-};
+using namespace std;
+
+// --- Brute Force Approach ---
+// Time Complexity: O(N^2)
+// Space Complexity: O(N)
+void solveBrute() {
+    // TODO: Implement brute force
+    // A naive approach exploring all possible states
+}
+
+// --- Optimal Approach ---
+// Time Complexity: O(N) or O(N log N)
+// Space Complexity: O(1) or O(N)
+void solveOptimal() {
+    // TODO: Implement optimal solution
+    // Utilize efficient data structures and algorithmic patterns
+}
 
 int main() {
-    MedianFinder mf;
-    mf.addNum(1);
-    mf.addNum(2);
-    cout << mf.findMedian() << endl; // 1.5
-    mf.addNum(3);
-    cout << mf.findMedian() << endl; // 2
-    mf.addNum(4);
-    cout << mf.findMedian() << endl; // 2.5
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // cout << "Testing Find Median from Data Stream" << endl;
+    // solveOptimal();
+    
     return 0;
 }
-```
+
+
