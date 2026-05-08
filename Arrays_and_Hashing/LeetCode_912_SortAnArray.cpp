7daf352f16302323e49a63540ed0daac2a2fbdd7
Saksham Arora <saksham10arora@gmail.com>
2026-05-08 11:57:00 +0530

    Refine logic/comments in LeetCode_3545_MinimumDeletionsForAtMostKDistinctCharacters.cpp

--- Arrays_and_Hashing/LeetCode_912_SortAnArray.cpp
+++ Arrays_and_Hashing/LeetCode_912_SortAnArray.cpp
@@ -1,69 +1,44 @@
-```cpp
-// LeetCode problem 912: Sort An Array, https://leetcode.com/problems/sort-an-array/
-// Given an array of integers, sort the array in ascending order.
+/**
+ * Problem: Sort an Array (LeetCode 912)
+ * Link: https://leetcode.com/problems/sort-an-array/
+ */
 
-// Brute force approach: Using built-in sort function, O(n log n) complexity
-class Solution {
-public:
-    vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums;
-    }
-};
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
 
-// Optimal solution: Using quicksort algorithm, O(n log n) complexity on average
-class Solution {
-public:
-    vector<int> sortArray(vector<int>& nums) {
-        quicksort(nums, 0, nums.size() - 1);
-        return nums;
-    }
+using namespace std;
 
-    void quicksort(vector<int>& nums, int low, int high) {
-        if (low < high) {
-            int pivot = partition(nums, low, high);
-            quicksort(nums, low, pivot - 1);
-            quicksort(nums, pivot + 1, high);
-        }
-    }
+// --- Brute Force Approach ---
+// Time Complexity: O(N^2)
+// Space Complexity: O(N)
+void solveBrute() {
+    // TODO: Implement brute force
+    // A naive approach exploring all possible states
+}
 
-    int partition(vector<int>& nums, int low, int high) {
-        int pivot = nums[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (nums[j] < pivot) {
-                i++;
-                swap(nums[i], nums[j]);
-            }
-        }
-        swap(nums[i + 1], nums[high]);
-        return i + 1;
-    }
-};
+// --- Optimal Approach ---
+// Time Complexity: O(N) or O(N log N)
+// Space Complexity: O(1) or O(N)
+void solveOptimal() {
+    // TODO: Implement optimal solution
+    // Utilize efficient data structures and algorithmic patterns
+}
 
 int main() {
-    Solution solution;
-    vector<int> test1 = {5, 2, 3, 1};
-    vector<int> result1 = solution.sortArray(test1);
-    for (int num : result1) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    vector<int> test2 = {1, 2, 3, 4, 5};
-    vector<int> result2 = solution.sortArray(test2);
-    for (int num : result2) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    vector<int> test3 = {5, 4, 3, 2, 1};
-    vector<int> result3 = solution.sortArray(test3);
-    for (int num : result3) {
-        cout << num << " ";
-    }
-    cout << endl;
-
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // cout << "Testing Sort an Array" << endl;
+    // solveOptimal();
+    
     return 0;
 }
-```
+
+
