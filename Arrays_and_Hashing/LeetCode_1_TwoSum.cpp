fed320aeadcf3ef9606e49a8794a53511524fb29
Saksham Arora <saksham10arora@gmail.com>
2026-06-09 12:33:00 +0530

    Refine logic/comments in LeetCode_1_TwoSum.cpp

--- Arrays_and_Hashing/LeetCode_1_TwoSum.cpp
+++ Arrays_and_Hashing/LeetCode_1_TwoSum.cpp
@@ -1,21 +1,13 @@
-/**
- * Problem: Two Sum (LeetCode 1)
- * Link: https://leetcode.com/problems/two-sum/
- * 
- * Description: Given an array of integers nums and an integer target, 
- * return indices of the two numbers such that they add up to target.
- */
+```cpp
+// LeetCode problem 1: Two Sum, https://leetcode.com/problems/two-sum/
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 
-using namespace std;
-
-// --- Brute Force ---
-// Time Complexity: O(N^2)
-// Space Complexity: O(1)
-vector<int> twoSumBrute(vector<int>& nums, int target) {
+// Brute force approach with O(n^2) complexity
+std::vector<int> twoSumBruteForce(std::vector<int>& nums, int target) {
     for (int i = 0; i < nums.size(); i++) {
         for (int j = i + 1; j < nums.size(); j++) {
             if (nums[i] + nums[j] == target) {
@@ -26,67 +18,35 @@ vector<int> twoSumBrute(vector<int>& nums, int target) {
     return {};
 }
 
-// --- Optimal Solution (using Hash Map) ---
-// Time Complexity: O(N)
-// Space Complexity: O(N)
-vector<int> twoSumOptimal(vector<int>& nums, int target) {
-    unordered_map<int, int> mpp;
+// Optimal solution with O(n) complexity
+std::vector<int> twoSumOptimal(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> numToIndex;
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
-        if (mpp.find(complement) != mpp.end()) {
-            return {mpp[complement], i};
+        if (numToIndex.find(complement) != numToIndex.end()) {
+            return {numToIndex[complement], i};
         }
-        mpp[nums[i]] = i;
+        numToIndex[nums[i]] = i;
     }
     return {};
 }
 
 int main() {
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
-    vector<int> result = twoSumOptimal(nums, target);
-    cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    std::vector<int> nums1 = {2, 7, 11, 15};
+    int target1 = 9;
+    std::vector<int> result1 = twoSumOptimal(nums1, target1);
+    std::cout << "Indices: " << result1[0] << ", " << result1[1] << std::endl;
+
+    std::vector<int> nums2 = {3, 2, 4};
+    int target2 = 6;
+    std::vector<int> result2 = twoSumOptimal(nums2, target2);
+    std::cout << "Indices: " << result2[0] << ", " << result2[1] << std::endl;
+
+    std::vector<int> nums3 = {3, 3};
+    int target3 = 6;
+    std::vector<int> result3 = twoSumOptimal(nums3, target3);
+    std::cout << "Indices: " << result3[0] << ", " << result3[1] << std::endl;
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+```
