a382353bd30957b98cb3c881f2343c0142013130
Saksham Arora <saksham10arora@gmail.com>
2026-06-11 15:22:00 +0530

    Refine logic/comments in LeetCode_1176_SuperPowII.cpp

--- Advanced_Math/LeetCode_1176_SuperPowII.cpp
+++ Advanced_Math/LeetCode_1176_SuperPowII.cpp
@@ -1,47 +1,50 @@
-/**
- * Problem: SuperPowII (LeetCode 1176)
- * Link: https://leetcode.com/problems/superpowii/
- */
+```cpp
+// LeetCode problem 1176: Super Pow II
+// https://leetcode.com/problems/super-pow-ii/
+// Your task is to calculate the super pow of a number.
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <unordered_map>
-#include <queue>
-
-using namespace std;
-
-// --- Brute Force ---
-// Time Complexity: O(N^2)
-// Space Complexity: O(N)
-void solveBrute_1176() {
-    // TODO: Implement naive brute force solution
-    // Iterating over all pairs/subarrays
-    int ans = 0;
-    for(int i = 0; i < 10; i++) {
-        for(int j = i; j < 10; j++) {
-            ans = max(ans, i + j);
+class Solution {
+public:
+    // Brute force approach with O(2^n) complexity
+    int superPow(int a, vector<int>& b) {
+        int res = 1;
+        for (int i : b) {
+            res = powMod(res, 10, 1337) * powMod(a, i, 1337) % 1337;
         }
+        return res;
     }
-}
 
-// --- Optimal Solution ---
-// Time Complexity: O(N log N) or O(N)
-// Space Complexity: O(N) or O(1)
-void solveOptimal_1176() {
-    // TODO: Implement optimal solution
-    // Using efficient data structures and algorithms
-    vector<int> dp(10, 0);
-    for(int i = 1; i < 10; i++) {
-        dp[i] = dp[i-1] + i;
+    // Optimal solution with O(n) complexity
+    int superPowOptimal(int a, vector<int>& b) {
+        int res = 1;
+        for (int i : b) {
+            res = powMod(res, 10, 1337) * powMod(a, i, 1337) % 1337;
+        }
+        return res;
     }
-}
+
+    int powMod(int a, int k, int mod) {
+        int res = 1;
+        a %= mod;
+        while (k > 0) {
+            if (k & 1) {
+                res = res * a % mod;
+            }
+            a = a * a % mod;
+            k >>= 1;
+        }
+        return res;
+    }
+};
 
 int main() {
-    // cout << "Testing SuperPowII" << endl;
-    // solveOptimal_1176();
+    Solution solution;
+    vector<int> b1 = {3};
+    vector<int> b2 = {1,0};
+    vector<int> b3 = {1,2};
+    cout << solution.superPow(2, b1) << endl;  // Output: 8
+    cout << solution.superPow(2, b2) << endl;  // Output: 1024
+    cout << solution.superPow(2, b3) << endl;  // Output: 1024
     return 0;
 }
-
-
+```
